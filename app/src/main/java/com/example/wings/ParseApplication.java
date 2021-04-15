7d675fae19774cb1ee9093a960e38b8ffdcd63b6
lmsiu <lmsiu@cpp.edu>
2021-04-15 01:47:41 -0700

    Registered models as Parse subclasses

--- app/src/main/java/com/example/wings/ParseApplication.java
+++ app/src/main/java/com/example/wings/ParseApplication.java
@@ -2,6 +2,10 @@ package com.example.wings;
 
 import android.app.Application;
 
+import com.example.wings.models.Buddy;
+import com.example.wings.models.BuddyRequest;
+import com.example.wings.models.BuddyTrip;
+import com.example.wings.models.TrustedContact;
 import com.example.wings.models.User;
 import com.parse.Parse;
 import com.parse.ParseObject;
@@ -14,6 +18,10 @@ public class ParseApplication  extends Application {
 
         // Register your parse models
         ParseObject.registerSubclass(User.class);
+        ParseObject.registerSubclass(Buddy.class);
+        ParseObject.registerSubclass(BuddyRequest.class);
+        ParseObject.registerSubclass(BuddyTrip.class);
+        ParseObject.registerSubclass(TrustedContact.class);
 
         // Initializes Parse SDK as soon as the application is created
         Parse.initialize(new Parse.Configuration.Builder(this)
