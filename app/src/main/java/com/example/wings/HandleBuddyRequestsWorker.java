98a8e846df9c69c3cac1e59561c7dfed83f0ca8d
jmnguyen1999 <jmnguyen1999@gmail.com>
2021-05-14 14:35:55 -0700

    Implementing tons of random stuff you guys can just lookseee

---
+++ app/src/main/java/com/example/wings/HandleBuddyRequestsWorker.java
@@ -0,0 +1,123 @@
+package com.example.wings;
+
+//Purpose:      To continuously listen to all BuddyRequests --> notify when someone has accepted, notify when you have received
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.wings.mainactivity.MainActivity;
+import com.example.wings.models.Buddy;
+import com.example.wings.models.BuddyRequest;
+import com.example.wings.models.User;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class HandleBuddyRequestsWorker extends Worker {
+    private static final String TAG = "HandlerBuddyRequestsWorker";
+    private static final String KEY_INITIAL_COUNT = "initialSizeReceivedRequests";  //this is to
+    private static final String KEY_CHANGED = "receivedRequestsChanged?";
+    private static final String KEY_NUM_RECEIVED = "responseTotalReceived";
+    private static final String KEY_APPROVED_REQUEST = "whichSentRequest?";
+    private static final String KEY_ISAPPROVED = "anyRequestsApproved?";
+
+    private CountDownLatch latch = new CountDownLatch(1);
+    private Context context;
+    private ParseUser currentUser;
+    private Buddy currentBuddyInstance;
+
+    public HandleBuddyRequestsWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.context = context;
+
+       /* currentUser = ParseUser.getCurrentUser();
+        currentBuddyInstance = (Buddy) currentUser.getParseObject(User.KEY_BUDDY);
+        try {
+            currentBuddyInstance.fetchIfNeeded();
+        } catch (ParseException e) {
+            Log.d(TAG, "in constructor: currentBuddyInstance couldn't be fetched");
+            e.printStackTrace();
+        }
+        latch.countDown();*/
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.d(TAG, "in doWork()");
+
+        //1.) Get the size of received requests
+        Data data = getInputData();
+        int initialNumRequests = data.getInt(KEY_INITIAL_COUNT, -2);
+        Log.d(TAG, "in doWork(): initialNumReq="+initialNumRequests);
+
+        currentUser = ParseUser.getCurrentUser();
+        currentBuddyInstance = (Buddy) currentUser.getParseObject(User.KEY_BUDDY);
+        try {
+            currentBuddyInstance.fetchIfNeeded();
+            latch.countDown();
+        } catch (ParseException e) {
+            Log.d(TAG, "in constructor: currentBuddyInstance couldn't be fetched");
+            e.printStackTrace();
+        }
+
+
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<BuddyRequest> receivedRequests = currentBuddyInstance.getList(Buddy.KEY_RECIEVEDREQUESTS);
+        Log.d(TAG, "in doWork() receivedRequests=" + receivedRequests.toString());
+        int numReceived = receivedRequests.size();
+        Log.d(TAG, "in doWork() numReceived=" + numReceived);
+        boolean numRequestsChanged = false;
+
+        if(numReceived != initialNumRequests && initialNumRequests > -1){
+            numRequestsChanged = true;
+        }
+
+/*
+        //2.) Get all sentRequests and check if any of them are approved:
+        List<BuddyRequest> sentRequests = currentBuddyInstance.getSentRequests();
+        Log.d(TAG, "in doWork() sentRequests=" + sentRequests.toString());   */
+        boolean isApproved = false;
+   /*     int approvedRequestIndex = -1;
+        for(int i = 0; i < sentRequests.size(); i++){
+            BuddyRequest currSentRequest = sentRequests.get(i);
+            try {
+                currSentRequest.fetchIfNeeded();
+
+                if(currSentRequest.getIsConfirmed()){
+                    Log.d(TAG, "in doWork() in isConfirmed");
+
+                    isApproved = true;
+             /*       approvedRequestIndex = i;
+                    break;
+                }
+            } catch (ParseException e) {
+                Log.d(TAG, "error fetching BuddyRequest");
+                e.printStackTrace();
+            }
+
+        }
+*/
+        Data output =  new Data.Builder()
+                .putBoolean(KEY_CHANGED, numRequestsChanged)
+                .putInt(KEY_NUM_RECEIVED, numReceived)
+                .putBoolean(KEY_ISAPPROVED, isApproved)
+                .putInt(KEY_APPROVED_REQUEST, 0/*approvedRequestIndex*/ )
+                .build();
+
+        return Result.success(output);
+
+    }
+}
--- app/src/main/java/com/example/wings/mainactivity/fragments/UserBuddyRequestsFragment.java
+++ app/src/main/java/com/example/wings/mainactivity/fragments/UserBuddyRequestsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
@@ -18,6 +19,12 @@ import android.widget.Toast;
 import com.example.wings.R;
 import com.example.wings.adapters.ChooseBuddyAdapter;
 import com.example.wings.mainactivity.MAFragmentsListener;
+import com.example.wings.models.Buddy;
+import com.example.wings.models.BuddyRequest;
+import com.example.wings.models.User;
+import com.example.wings.models.WingsGeoPoint;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
@@ -33,6 +40,7 @@ public class UserBuddyRequestsFragment extends Fragment {
 
     private MAFragmentsListener listener;
     private ParseUser currentUser = ParseUser.getCurrentUser();
+    private ParseGeoPoint currentLocation;
 
     //Views:
     private RecyclerView rvSentRequests;
@@ -47,6 +55,9 @@ public class UserBuddyRequestsFragment extends Fragment {
     List<Double> distanceFromSent;
     List<ParseUser> peopleReceivedFrom;
     List<Double> distanceFromReceivers;
+
+    List<BuddyRequest> sentRequests;
+    List<BuddyRequest> receivedRequests;
 
     public UserBuddyRequestsFragment() {}
 
@@ -95,6 +106,13 @@ public class UserBuddyRequestsFragment extends Fragment {
         rvReceivedRequests = view.findViewById(R.id.rvReceived);
         btnBack = view.findViewById(R.id.btnBack);
 
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //just go to homeFragment bc why not:
+                listener.toHomeFragment();
+            }
+        });
         peopleSentTo = new ArrayList<>();
         distanceFromSent = new ArrayList<>();
         peopleReceivedFrom = new ArrayList<>();
@@ -102,18 +120,154 @@ public class UserBuddyRequestsFragment extends Fragment {
 
 
         //Set on click listener:
-        //2.) Create an ChooseBuddyAdapter.OnClickListener to create a ChooseBuddyAdapter:
+        //2.) Create an ChooseBuddyAdapter.OnClickListener to create a ChooseBuddyAdapter -- will be used by both recycler views as tapping on any row does the same thing
         ChooseBuddyAdapter.OnClickListener onClickListener = new ChooseBuddyAdapter.OnClickListener() {
             @Override
             public void onClick(int position) {
                 //I think position = corresponds to the indexes in the List<> models but may be backwards or something:
-                //TODO: open a simple yes or no dialog box
+                //TODO: go to that one potential buddies fragment
                 Toast.makeText(getContext(), "You clicked on a request, I should show a dialog rn", Toast.LENGTH_SHORT).show();
             }
         };
 
         sentRequestsAdapter = new ChooseBuddyAdapter(getContext(), peopleSentTo, distanceFromSent, onClickListener);
+        receivedRequestAdapter = new ChooseBuddyAdapter(getContext(), peopleReceivedFrom, distanceFromReceivers, onClickListener);
 
-        //TODO: this is not done like at all, please do not use. I stopped in the middle of it due to time
+        //set up both rv's:
+        rvSentRequests.setAdapter(sentRequestsAdapter);
+        rvSentRequests.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        rvReceivedRequests.setAdapter(receivedRequestAdapter);
+        rvReceivedRequests.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        //initialize the current user's currentLocation to find distance:
+        WingsGeoPoint currLocationGeoPoint = (WingsGeoPoint) currentUser.getParseObject(User.KEY_CURRENTLOCATION);
+        try {
+            currLocationGeoPoint.fetchIfNeeded();
+            currentLocation = currLocationGeoPoint.getLocation();
+
+        } catch (ParseException e) {
+            Log.d(TAG, "onViewCreated(): error trying to fetch the WingsGeoPoint currLocation");
+            e.printStackTrace();
+        }
+
+        getRequests();      //initializes sentRequests and receivedRequests fields
+        getParseUsers(sentRequests, false);                  //As the sentRequests, I want to display the receivers of who I sent my requests to --> false  //Initializes all models depending on looking for sender or receiver
+        getParseUsers(receivedRequests, true);
     }
+
+    //Purpose:      To initialize/get the sentRequests and receivedRequests
+    public void getRequests(){
+        Buddy buddyInstance = (Buddy) currentUser.getParseObject(User.KEY_BUDDY);
+        try {
+            buddyInstance.fetchIfNeeded();
+
+            sentRequests = buddyInstance.getSentRequests();
+            receivedRequests = buddyInstance.getReceivedRequests();
+        } catch (ParseException e) {
+            Log.d(TAG, "getRequests(): error fetching buddy instance, e=" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    //Purpose:      take out the ParseUser and find the distances b/w our currentLocation and the other ParseUser's currentLocation, assume our currentLocation is initialized.
+    public void getParseUsers(List<BuddyRequest> requestList, boolean getSender){
+        List<ParseUser> result = new ArrayList<>();
+        List<Double> distancesList = new ArrayList<>();         //use setter methods to set them appropriately at the end
+
+        for(int i = 0; i < requestList.size(); i++) {
+            BuddyRequest currentRequest =  requestList.get(i);
+            if (getSender) {
+                try {
+                    currentRequest.fetchIfNeeded();
+
+                    //Get the senders of each BuddyRequest
+                    Buddy currentSenderBuddy = currentRequest.getSender();
+                    currentSenderBuddy.fetchIfNeeded();
+                    ParseUser currentSenderUser = currentSenderBuddy.getUser();
+
+                    //Add found user to List:
+                    result.add(currentSenderUser);
+
+
+                    //Now that we have the user --> find the distance bewteen:
+                    //Find the ParseGeoPoint of the otherUser:
+                    WingsGeoPoint otherCurrLocationGeoPoint = (WingsGeoPoint) currentSenderUser.getParseObject(User.KEY_CURRENTLOCATION);
+                    otherCurrLocationGeoPoint.fetchIfNeeded();
+                    ParseGeoPoint otherCurrLocation = otherCurrLocationGeoPoint.getLocation();
+
+                    double currDistance = currentLocation.distanceInKilometersTo(otherCurrLocation)*1000;     //*1000 to get meters
+
+                    //Add the found distance to our List<Double>
+                    distancesList.add(currDistance);
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+
+            } else {
+                //Get the receivers of each BuddyRequest
+                //Get the senders of each BuddyRequest
+                try {
+                    currentRequest.fetchIfNeeded();
+
+                    Buddy currentReceiverBuddy = currentRequest.getReceiver();
+                    currentReceiverBuddy.fetchIfNeeded();
+                    ParseUser currentReceiverUser = currentReceiverBuddy.getUser();
+
+                    //Add found user to List:
+                    result.add(currentReceiverUser);
+
+
+                    //Now that we have the user --> find the distance bewteen:
+                    //Find the ParseGeoPoint of the otherUser:
+                    WingsGeoPoint otherCurrLocationGeoPoint = (WingsGeoPoint) currentReceiverUser.getParseObject(User.KEY_CURRENTLOCATION);
+                    otherCurrLocationGeoPoint.fetchIfNeeded();
+                    ParseGeoPoint otherCurrLocation = otherCurrLocationGeoPoint.getLocation();
+
+                    double currDistance = currentLocation.distanceInKilometersTo(otherCurrLocation)*1000;     //*1000 to get meters
+
+                    //Add the found distance to our List<Double>
+                    distancesList.add(currDistance);
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //Once results are initialized --> set them in the class fields:
+        if(!getSender){         //If I got the receivers -->
+            //save to the sender models:
+            setPeopleSentTo(result);
+            setDistanceFromSent(distancesList);
+        }
+        else{
+            //save to the receiver models:
+            setPeopleReceivedFrom(result);
+            setDistanceFromReceivers(distancesList);
+        }
+    }
+
+    private void setPeopleSentTo(List<ParseUser> result) {
+        peopleSentTo.addAll(result);
+        sentRequestsAdapter.notifyDataSetChanged();
+    }
+    private void setDistanceFromSent(List<Double> distancesList) {
+        distanceFromSent.addAll(distancesList);
+        sentRequestsAdapter.notifyDataSetChanged();
+    }
+
+    private void setPeopleReceivedFrom(List<ParseUser> result) {
+        peopleReceivedFrom.addAll(result);
+        receivedRequestAdapter.notifyDataSetChanged();
+    }
+
+    private void setDistanceFromReceivers(List<Double> distancesList) {
+        distanceFromReceivers.addAll(distancesList);
+        receivedRequestAdapter.notifyDataSetChanged();
+    }
+
+
 }
